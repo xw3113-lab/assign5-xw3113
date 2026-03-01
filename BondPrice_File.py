dc6edd8d3b6552c4a74ea38e2fa698b7f325318c
xw3113-lab <xw3113@columbia.edu>
2026-03-01 18:55:41 -0500

    Refactor getBondPrice to calculate bond price
    Refactored getBondPrice function to calculate bond price based on cash flows and present value.

--- BondPrice_File.py
+++ BondPrice_File.py
@@ -1,8 +1,11 @@
 import numpy as np
 
-def getBondPrice(y, face, couponRate, m, ppy=1):
-    if ppy == 1:
-        x = 2170604
-    if ppy == 2:
-        x = 2171686
-    return(x)
+def getBondPrice(y, face, couponRate, m, ppy = 1):
+    pvcfsum = 0
+    cf = couponRate * face
+    for t in range(1, (ppy *m) +1):
+        pv = (1+ y/ppy) **(-t)
+        pvcf= pv*cf/ppy
+        pvcfsum += pvcf
+    bondprice = pvcfsum + pv*face
+    return(bondprice)
