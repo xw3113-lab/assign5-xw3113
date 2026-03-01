dbceb70a051e499e3a536eecde30b4390ba3d086
xw3113-lab <xw3113@columbia.edu>
2026-03-01 02:24:38 -0500

    Initial commit

---
+++ BondDuration_File.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def getBondDuration(y, face, couponRate, m, ppy = 1):
+    if ppy == 1:
+        x = 8.51
+    if ppy == 2:
+        x = 8.42
+    return(x)
---
+++ README.md
@@ -0,0 +1,1 @@
+# Py-hw2-StarterFiles
